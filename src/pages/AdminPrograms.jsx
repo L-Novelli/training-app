3aafa3110c99740503b1f2ab1fc0667ed326ec95
L-Novelli <l.novelli@outlook.com>
2026-08-20 20:00:23 -0300

    cchanged supabase

--- src/pages/AdminPrograms.jsx
+++ src/pages/AdminPrograms.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { supabase } from '../supabaseClient'
+import { supabase } from '../lib/supabaseClient'
 
 export default function AdminPrograms() {
   const [programs, setPrograms] = useState([])
